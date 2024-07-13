3a4823022b6279387f3d256b01ea6d568d4ba546
Charif Mews <charif@westaysafe.com>
2024-07-13 14:26:35 +0200

    Working chat

--- lib/secret_quest_web/live/home.live.ex
+++ lib/secret_quest_web/live/home.live.ex
@@ -5,17 +5,49 @@ defmodule SecretQuestWeb.HomeLive do
     socket =
       assign(socket, :address, params["address"])
       |> stream(:presences, [])
+      |> stream(:messages, [])
 
     socket =
       if connected?(socket) do
         SecretQuestWeb.Presence.track_user(params["address"], %{id: params["address"]})
         SecretQuestWeb.Presence.subscribe()
-        stream(socket, :presences, SecretQuestWeb.Presence.list_online_users())
+        SecretQuestWeb.Endpoint.subscribe("tower:lobby")
+        stream(socket, :presences, SecretQuestWeb.Presence.list_online_users() |> Enum.uniq())
       else
         socket
       end
 
     {:ok, socket}
+  end
+
+  def handle_event("send_message", %{"message" => message}, socket) do
+    message_data = %{
+      user: socket.assigns.address,
+      body: message,
+      timestamp: DateTime.utc_now(),
+      id: Ecto.UUID.generate()
+    }
+
+    Phoenix.PubSub.broadcast(SecretQuest.PubSub, "tower:lobby", %{
+      event: "message",
+      payload: %{message: message_data}
+    })
+
+    {:noreply,
+     socket
+     |> stream(:presences, SecretQuestWeb.Presence.list_online_users())
+     |> stream_insert(:messages, message_data)}
+  end
+
+  def handle_info(%{event: "message", payload: %{message: message = %{user: user}}}, socket) do
+    if user != socket.assigns.address do
+      {:noreply,
+       socket
+       |> stream(:presences, SecretQuestWeb.Presence.list_online_users())
+       |> stream_insert(:messages, message)}
+    else
+      {:noreply, socket}
+    end
   end
 
   def handle_info({SecretQuestWeb.Presence, {:join, _presence}}, socket) do
