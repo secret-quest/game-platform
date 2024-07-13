9c945a9889fd5b36ca364a573ece93309b565564
Charif Mews <charif@westaysafe.com>
2024-07-13 08:39:48 +0200

    add openai deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule SecretQuest.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:openai_ex, "~> 0.8.0"}
     ]
   end
 
